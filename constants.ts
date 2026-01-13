df02f632862a702af5d32612f0e560b1bcb46bf1
rentaguide <EREZ@RENT-A-GUIDE.CO.IL>
2026-01-13 13:11:27 +0200

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,21 @@
+
+export const INITIAL_VOUCHER_NUMBER = 8000;
+
+export const DEFAULT_SERVICES = [
+  "City Tour",
+  "Airport Transfer",
+  "Museum Visit",
+  "Hiking Guide",
+  "Culinary Tour",
+  "Night Tour",
+  "Private Chauffeur"
+];
+
+export const DEFAULT_SUPPLIERS = [
+  "Hotel Hilton",
+  "Restaurant David",
+  "Transport Group A",
+  "Guide Association"
+];
+
+export const STORAGE_KEY = "rent_a_guide_data";
